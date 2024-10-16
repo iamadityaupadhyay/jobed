99d048238a3e98ac5cd079676d8f4f025ad98724
Aditya Upadhyay <uaditya219@gmail.com>
2024-10-17 01:13:19 +0530

    admin panel

--- jobed/models.py
+++ jobed/models.py
@@ -4,7 +4,7 @@ from cloudinary.models import CloudinaryField
 class UserModel(AbstractUser):
      image = CloudinaryField('image',null=True ,blank=True)
      
-     mobile_number =models.IntegerField(null=True, blank=True)
+     mobile_number =models.CharField(max_length=200,null=True, blank=True)
      type =models.CharField(choices=[('Recuiter','Recuiter'),('Student','Student')] ,max_length=50 ,null=True, blank=True)
      def __str__(self):
          return self.first_name
