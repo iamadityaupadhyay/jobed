2382847696034e3f7f7b1471a5e56c795dafee4e
Aditya Upadhyay <uaditya219@gmail.com>
2024-10-17 01:16:48 +0530

    admin panel

--- jobed/models.py
+++ jobed/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import *
 from cloudinary.models import CloudinaryField
 class UserModel(AbstractUser):
      image = CloudinaryField('image',null=True ,blank=True)
-     
      mobile_number =models.CharField(max_length=200,null=True, blank=True)
      type =models.CharField(choices=[('Recuiter','Recuiter'),('Student','Student')] ,max_length=50 ,null=True, blank=True)
      def __str__(self):
