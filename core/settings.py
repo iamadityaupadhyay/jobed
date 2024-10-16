6f545462220a6c526a7b3171720f11f8faf77ea9
Aditya Upadhyay <uaditya219@gmail.com>
2024-10-17 00:39:18 +0530

    admin panel

--- core/settings.py
+++ core/settings.py
@@ -80,7 +80,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Claudinary
 from dotenv import load_dotenv
