9c66dce3d3ece20d21d5bd0d633f5e84b98c7c69
Aditya Upadhyay <uaditya219@gmail.com>
2024-10-17 23:42:53 +0530

    new

--- jobed/serializers.py
+++ jobed/serializers.py
@@ -9,14 +9,30 @@ class UserSerializer(serializers.ModelSerializer):
         fields = "__all__"
     
     def create(self, validated_data):
+        try:
+            if 'image' in validated_data:
+                image = validated_data.pop('image')
+                upload_response = cloudinary.uploader.upload(image)
+                validated_data['image'] = upload_response['url']
+        except Exception as e:
+            raise serializers.ValidationError({"image": "Failed to upload image to Cloudinary."})
         
-        if 'image' in validated_data:
-            print('image')
-            image = validated_data.pop('image')
-            upload_response = cloudinary.uploader.upload(image)
-            validated_data['image'] = upload_response['url']
-        password = validated_data.pop('password')
-        user = UserModel(**validated_data)
-        user.set_password(password)
-        user.save()
-        return user
+        # Handle many-to-many fields (if any)
+        groups = validated_data.pop('groups', None)
+        permissions = validated_data.pop('user_permissions', None)
+        
+        password = validated_data.pop('password', None)
+        if password:
+            user = UserModel(**validated_data)
+            user.set_password(password)
+            user.save()
+
+            # Now add the many-to-many relationships using set()
+            if groups:
+                user.groups.set(groups)
+            if permissions:
+                user.user_permissions.set(permissions)
+
+            return user
+        else:
+            raise serializers.ValidationError({"password": "Password is required."})
