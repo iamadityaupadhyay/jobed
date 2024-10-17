98500d7d6b9f78f3c7d38657f652f0684d7182be
Aditya Upadhyay <uaditya219@gmail.com>
2024-10-17 10:14:58 +0530

    admin panel

--- jobed/urls.py
+++ jobed/urls.py
@@ -4,6 +4,6 @@ from .views import *
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    path("register",register)
+    path("register/",register)
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
